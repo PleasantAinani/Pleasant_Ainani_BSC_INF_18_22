ca64f9a1747005374e9a645d8b172e55acd7e159
PleasantAinani <167509855+PleasantAinani@users.noreply.github.com>
2024-04-27 17:25:15 +0200

    Update DynamicMemory.cpp

--- _Lab_task6/DynamicMemory.cpp
+++ _Lab_task6/DynamicMemory.cpp
@@ -3,17 +3,22 @@ using namespace std;
 
 int main(){
 
+/*dynamically allocation of memory to variables*/
+
 int* num= new int;
 string* some= new string;
 
+/*getting user input*/
 cout<<"Enter an integer :"<<endl;
 cin >>*num;
 cout<<"Enter a string :"<<endl;
 cin >> *some;
 
+/*outputs the input*/
 cout<<"The number is :"<<*num <<endl;
 cout<<"The string is :"<<*some <<endl;
 
+/*deallocates memory*/
 delete num;
 delete some;
 
