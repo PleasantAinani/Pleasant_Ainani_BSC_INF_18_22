b51de714e77ebe692a21edc6a5260d7e2818bd83
PleasantAinani <167509855+PleasantAinani@users.noreply.github.com>
2024-05-03 09:19:55 +0200

    Update, a file that swaps two numbers!

---
+++ lab-task7.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int swapNumbers(int a, int b);
+
+int main(int argc, char *argv[])
+{
+int varA = 25;
+int varB = 100;
+
+cout<< "varA before swap: "<< varA<< endl;
+cout<< "varB before swap: "<< varB<< endl;
+		
+	swapNumbers(varA, varB);
+cout<<"varA after swap : "<<varA<<endl;
+cout<<"varB after swap: "<<varB<<endl;
+
+return 0;
+}
+
+int swapNumbers(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    
+    return a, b;
+}
+    
